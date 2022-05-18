f3309118c35fc9c02664b606f60a035093854592
“courtneylake” <courtney@giffgaff.co.uk>
2022-05-18 11:46:53 +0100

    [TOOL-1060] - Enabling scram in the module

--- main.tf
+++ main.tf
@@ -182,7 +182,7 @@ resource "aws_msk_scram_secret_association" "default" {
   count = var.scram_enabled ? 1 : 0
 
   cluster_arn     = aws_msk_cluster.this.arn
-  secret_arn_list = var.client_sasl_scram_secret_association_arns
+  secret_arn_list = [aws_secretsmanager_secret.example.arn]
 }
 
 resource "aws_secretsmanager_secret" "secret" {
